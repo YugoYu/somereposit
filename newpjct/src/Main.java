9268fba6817a6dd77d580fccc6b8f05e3c364702
Mihail Kislov <Mihael22831@gmail.com>
2023-02-09 20:59:11 +0300

    BugFIx Main.java

--- newpjct/src/Main.java
+++ newpjct/src/Main.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         String grnt = String.join(",","Mrat ");
     }
-}
-=======
         String grnt = String.join(",","Mrat ","Shakur ","Shakir ");
     }
->>>>>>> 2d7e809595e105f7b7960078ae432f5af609bc5a
